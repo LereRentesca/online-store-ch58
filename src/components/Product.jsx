d847271a9a2da1ade091ab41c88b2ce18b75ec67
LereRentesca <esteban.renteria@uabc.edu.mx>
2025-07-01 19:56:17 -0700

    Class #3

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -1,22 +1,25 @@
 import "./Product.css";
 import QuantityPicker from "./QuantityPicker";
 
-function Product() {
+function Product(props) {
+
+    console.log(props);
+
     return (
         <div className="product">
-            <span> ID: 0000</span>
-            <img src="https://picsum.photos/250/200" alt=""/>
-            <h3>I'm a product!</h3>
+            <span> ID: {props.data._id}</span>
+            <img src={"./images/"+props.data.image} alt=""/>
+            <h3>{props.data.title}</h3>
             <div className="price-section">
                 <div className="prices">
-                    <labe>$total</labe>
-                    <label>$price</label>
+                    <label>Total: $0.00</label>
+                    <label>Price: ${props.data.price}</label>
                 </div>
                 <div>
                     <QuantityPicker/>
                 </div> 
             </div>
-            <div>
+            <div className="add-button">
                 <button className="btn btn-sm btn-success">Add</button>
             </div>
         </div>
--- src/pages/Catalog.jsx
+++ src/pages/Catalog.jsx
@@ -1,18 +1,33 @@
 import "./Catalog.css";
 import Product from "../components/Product";
+import DataService from "../services/DataService";
+import { useState, useEffect } from "react";
 
 function Catalog() {
 
+    const [products, setProducts] = useState([]);
+
+    useEffect(function(){
+        loadCatalog();
+    },[]);
+
+    function loadCatalog() {
+        let service = new DataService();
+        let prods = service.getProducts();
+        setProducts(prods);
+    }
+
     return (
         <div className='main-content'>
-            <h1>This is our catalog:</h1>
-            <Product/>
-            <Product/>
-            <Product/>
-            <Product/>
-            <Product/>
-            <Product/>
-
+            <h3>We have {products.length} new products for you!</h3>
+            <br/>
+            <div className="catalog-items">
+                {
+                    products.map((item) => (
+                        <Product key={item._id} data={item}></Product>
+                    ))
+                }
+            </div>
         </div>
     );
 }
